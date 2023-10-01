bdda8402343579351e265fd17282e2d5d63896cb
b-haley <146177619+b-haley@users.noreply.github.com>
2023-10-01 16:37:42 +0300

    Update functions.h

--- functions.h
+++ functions.h
@@ -1,1 +1,15 @@
+#pragma once
+// Подключение необходимых библиотек и объявление функций
+#include <vector>
+#include <iostream>
+#include <ctime>
+#include <fstream>
+
+enum { yes = 1, no = 2 };
+using namespace std;
+
+vector<int> selectionSort(vector<int> array);
+int isNumber();
+int isNumber(int i, int j);
+void saveArray(vector<int> array, vector<int> sortedArray);
 
